b69fe0ae7aac31a6918a13d4c72791bec0a92afb
Abhishek Sahani <sahani199890@gmail.com>
2023-03-14 20:50:15 +0530

    Car_Service_Garage

--- src/Inputs.java
+++ src/Inputs.java
@@ -8,7 +8,8 @@ public class Inputs {
     Car vehicle;
     Scanner sc=new Scanner(System.in);
     public void selectCar(){
-        System.out.println("Please select the your car from [Benz, HATCHBACK, SUV, Sedan] :");
+        System.out.print("Please select the your car from [Benz, HATCHBACK, SUV, Sedan] :");
+
         String car1=sc.next();
         String car=car1.toLowerCase();
         if(car.equals("benz")){
@@ -29,13 +30,31 @@ public class Inputs {
         }
     }
     public void noOfServices(){
-        System.out.println("Please select no.of services needed :");
-        numberOfServices=sc.nextInt();
+        System.out.print("Please select no.of services needed :");
+        numberOfServices = sc.nextInt();
+        if(numberOfServices>5){
+            System.out.println("max services are only 5");
+            System.out.print("Please select no.of services needed :");
+            numberOfServices = sc.nextInt();
+        }
     }
     public void services() {
-        System.out.println("Please select service codes [BS01,EF01,CF01,BF01,GF01] :");
+        System.out.print("Please select service numbers \n1-->BS01 \n2-->EF01\n3-->CF01\n4-->BF01\n5-->GF01\n");
         for(int i=0;i<numberOfServices;i++){
-            list.add(sc.next());
+            switch (sc.nextInt()){
+                case 1:list.add("BS01");
+                break;
+                case 2:list.add("EF01");
+                break;
+                case 3:list.add("CF01");
+                break;
+                case 4:list.add("BF01");
+                break;
+                case 5:list.add("GF01");
+                break;
+                default:System.out.println("you cannot choose above 5 services ,choose again");
+                i--;
+            }
         }
     }
 }
